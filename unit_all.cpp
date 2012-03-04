1908946afb769e1b2318e2d05099d1679638e5f6
pennyliang <mgigabyte@gmail.com>
2012-03-04 18:34:56 +0800

    add auto build

---
+++ unit_all.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <gtest/gtest.h>
+//#include "/home/lbin/weibo/social/gtest-1.6.0/include/gtest/gtest.h"
+
+//#include "andrewng-problem-set3/regulation.h"
+
+TEST( HW,andrewng_problem_set3 )
+{
+//	progress("./andrewng-problem-set3/x.dat","andrewng-problem-set3/y.dat");
+        EXPECT_TRUE( true );
+}
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
